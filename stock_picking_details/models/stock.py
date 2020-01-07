73292045fc37c392a9900d823b3589ae469f14e5
mariodmoreno <mariodmorenosoltic@gmail.com>
2020-01-07 17:21:43 -0300

    [ADD] [stock_picking_details]

---
+++ stock_picking_details/models/stock.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
+import logging
+_logger = logging.getLogger(__name__)
+
+class stockPicking(models.Model):
+    
+    _inherit = 'stock.picking'
+
+    elevator = fields.Boolean(string='Elevator', required=True, default=True)
+    staircase = fields.Boolean(string='Staircase', required=True, default=True)
+    exterior_access = fields.Boolean(string='Exterior Access', required=True, default=True)
+    req_authorization = fields.Boolean(string='Req Authorization', required=True, default=True)
