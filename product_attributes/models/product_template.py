e0532555cff597929670694d17d9c95b68b1f6bc
Jhone Mendez <jhone0901@gmail.com>
2020-03-20 10:41:34 -0300

    [UPD] [product_attributes] & [sale_order_workflow] new design flow

--- product_attributes/models/product_template.py
+++ product_attributes/models/product_template.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import itertools
-import psycopg2
-
-from odoo.addons import decimal_precision as dp
-
-from odoo import api, fields, models, tools, _
-from odoo.exceptions import ValidationError, RedirectWarning, except_orm
-from odoo.tools import pycompat
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     attachment_ids = fields.Many2many(
-        'ir.attachment', 'product_ir_attachments_rel','template_id', 'attachment_id', 'Attachments')
+        "ir.attachment",
+        "product_ir_attachments_rel",
+        "template_id",
+        "attachment_id",
+        "Attachments",
+    )
