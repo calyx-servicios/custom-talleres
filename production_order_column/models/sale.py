a0ba8394303a475aa11243159fd87bea56a642cd
DarwinAndrade <andres.63.d@gmail.com>
2022-04-04 13:43:08 -0300

    upd

--- production_order_column/models/sale.py
+++ production_order_column/models/sale.py
@@ -11,4 +11,5 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    production_order = fields.Char(string="Production Order")
+    # production_order = fields.Char(string="Production Order")
+    production_order = fields.Many2one('mrp.production', string="Production Order")
