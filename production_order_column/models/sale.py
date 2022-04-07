73d3b648cb708cf258f0ac1e0ffbff9bfecb7c78
DarwinAndrade <andres.63.d@gmail.com>
2022-04-07 09:01:35 -0300

    [ADD][production_order_column] Incidence nbr./s.: ODOO-2904.  - Add production order column

--- production_order_column/models/sale.py
+++ production_order_column/models/sale.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, fields, models, _
 from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
 import logging
@@ -11,5 +8,4 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # production_order = fields.Char(string="Production Order")
     production_order = fields.Many2one('mrp.production', string="Production Order")
