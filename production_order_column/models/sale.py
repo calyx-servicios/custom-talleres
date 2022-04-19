8e2dc6b9df0688be42882ba20f00061404db8f3a
DarwinAndrade <andres.63.d@gmail.com>
2022-04-19 11:52:25 -0300

    [ADD][talleres_view_modifications] Incidence nbr./s.: ODOO-3549.  - hide fields in the contacts and sales modules

---
+++ production_order_column/models/sale.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', required=True,
+                                   readonly=True,
+                                   states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+                                   help="Pricelist for current sales order.",
+                                   default=lambda s: s.env['product.pricelist'].search([], limit=1))
