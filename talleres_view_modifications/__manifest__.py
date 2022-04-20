2e685aa4230535ab01cd69cbb7d464dd05bd53b1
DarwinAndrade <andres.63.d@gmail.com>
2022-04-20 16:17:52 -0300

    [ADD][talleres_view_modifications] Incidence nbr./s.: ODOO-3549.  - Hide Fields in Customer and Order

---
+++ talleres_view_modifications/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Talleres View Modifications',
+    'summary': '''Hide fields in the contacts and sales module''',
+    'version': '11.0.1',
+    'category': 'Tools',
+    'author': "Calyx",
+    'maintainers': ["DarwinAndrade"],
+    'website': 'www.calyxservicios.com.ar',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale', 'crm', 'base', 'sale_order_workflow'
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'view/res_partner_view.xml',
+        'view/sale_order_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
