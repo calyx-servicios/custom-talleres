deb09d068391adb3b7f03c9fdebbc271cf41fb0b
DarwinAndrade <81753754+DarwinAndrade@users.noreply.github.com>
2022-04-21 11:34:20 -0300

    [ADD][stock_picking_custom_report] (#11)
    Incidence nbr./s.: ODOO-2906.
     - Changes in delivery order report
    
    Co-authored-by: DarwinAndrade <andres.63.d@gmail.com>

---
+++ stock_picking_custom_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock Picking Custom Report",
+    "summary": """
+        Changes in delivery order report """,
+    "description": """
+    """,
+    "author": "Calyx",
+    "maintainers": ["DarwinAndrade"],
+    "website": "http://www.calyxservicios.com.ar",
+    "category": "Tools",
+    "version": "11.0.1.0.1",
+    "depends": ["stock"],
+    "data": [
+        "report/report_deliveryslip.xml",
+        "report/stock_report_views.xml",
+        "views/stock_picking_views.xml",
+    ]
+}
