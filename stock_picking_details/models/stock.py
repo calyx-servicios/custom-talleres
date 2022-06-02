587463302484454b7208adafbc2d297df05a16ae
gabbii <gperez@calyxservicios.com.ar>
2022-06-02 12:22:45 -0300

    [ODOO-3939][IMP][stock_picking_details] - IMP amount residual in validate.

--- stock_picking_details/models/stock.py
+++ stock_picking_details/models/stock.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
 import logging
 
 _logger = logging.getLogger(__name__)
 
-
 class StockPicking(models.Model):
-
     _inherit = "stock.picking"
+    
     elevator = fields.Boolean(string="Elevator", required=True, default=False)
     staircase = fields.Boolean(
         string="Staircase", required=True, default=False
@@ -20,34 +18,40 @@ class StockPicking(models.Model):
     )
     req_authorization = fields.Boolean(
         string="Req Authorization", required=True, default=False
-    )
-    
+    )    
     calcule_amount_residual = fields.Float(
         string="Amount Residual",
-        compute="_compute_amount_residual",
-        store=True
+        compute="_compute_amount_residual"
     )
           
-    @api.one
+    @api.model
     def _compute_amount_residual(self):
-        i = 0
-        total_amount_residual = 0.00
-        double_orders = []
-        if self.origin:
-            origin = self.origin.split()
-            simple_orders = self.origin.split()
-            for order in origin:
-                if order == "-":
-                    double_orders += [origin[i-1] + " " + order + " " + origin[i+1]]
-                    simple_orders.remove(origin[i-1])
-                    simple_orders.remove(order)
-                    simple_orders.remove(origin[i+1])
-                i += 1
-            
-            simple_orders.extend(double_orders)
-            for order in simple_orders:
-                record = self.env['sale.order'].search([('name', '=', order)])
-                if record.calcule_amount_residual:
-                    total_amount_residual += record.calcule_amount_residual
+        for rec in self:
+            i = 0
+            total_amount_residual = 0.00
+            double_orders = []
+            if rec.origin:
+                origin = rec.origin.split()
+                simple_orders = rec.origin.split()
+                for order in origin:
+                    if order == "-":
+                        double_orders += [origin[i-1] + " " + order + " " + origin[i+1]]
+                        simple_orders.remove(origin[i-1])
+                        simple_orders.remove(order)
+                        simple_orders.remove(origin[i+1])
+                    i += 1
+                
+                simple_orders.extend(double_orders)
+                for order in simple_orders:
+                    record = self.env['sale.order'].search([('name', '=', order)], limit=1)
+                    if record and record.calcule_amount_residual:
+                        total_amount_residual += record.calcule_amount_residual
+    
+            rec.calcule_amount_residual = total_amount_residual
 
-        self.calcule_amount_residual = total_amount_residual
+    def button_validate(self):
+        if self.calcule_amount_residual > 0:
+            raise UserError(_('You still have balance to pay on this order!'))
+
+        return super().button_validate()
+
