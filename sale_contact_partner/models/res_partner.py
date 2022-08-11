b2d1a509e7ca7d2058be8d127dbd1b3ab6403772
lucianobaleani <lucianobaleani94@gmail.com>
2022-08-11 13:35:55 -0300

    [ODOO-4191][UPD][sale_contact_partner]Constrains added to check email validation

--- sale_contact_partner/models/res_partner.py
+++ sale_contact_partner/models/res_partner.py
@@ -1,8 +1,17 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = "res.partner"
 
     sale_zone_ids = fields.Many2one(comodel_name="sale.order.zone")
-    
+
+
+    @api.constrains("email")
+    def _check_valid_email(self):
+        for record in self:
+            if " " in record.email:
+                raise UserError("Email cannot have spaces ' '")
+            if "@" not in record.email or ".com" not in record.email:
+                raise UserError("Please introduce a valid email")
