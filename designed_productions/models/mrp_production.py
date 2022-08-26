efd04e7ab514f33d37d2d06026bbfc2af1e0b109
lucianobaleani <lucianobaleani94@gmail.com>
2022-08-26 15:42:45 -0300

    [ODOO-4371][UPD][designed_productions]Changes related when a production order has the same product but with different variant

--- designed_productions/models/mrp_production.py
+++ designed_productions/models/mrp_production.py
@@ -5,15 +5,13 @@ class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
 
-    design = fields.Boolean(compute="_get_design", store=True)
+    design = fields.Boolean()
 
 
-    @api.depends("sale_id.order_line.to_design")
-    def _get_design(self):
-        for record in self:
-            for product in record.sale_id.order_line:
-                if product.to_design == True:
-                    record.update({"design": True})
-                else:
-                    record.update({"design": False})
-                    
+    @api.model
+    def create(self, vals):
+        res = super(MrpProduction, self).create(vals)
+        line = res.sale_id.order_line[len(res.sale_id.production_ids)-1]
+        res.design = line.to_design
+        return res
+        
