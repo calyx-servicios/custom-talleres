eaf8a8f13ce40931c2101818c0ce7cebcc157d71
lucianobaleani <lucianobaleani94@gmail.com>
2022-09-05 13:18:59 -0300

    [ODOO-4563][FIX][sale_date_filter]Fix the problem removing the time in sale date and quotation date in the filter view [UPD][search_assistant_custom]Remove the functions associated with the date_order_date field

--- sale_date_filter/models/sale_report.py
+++ sale_date_filter/models/sale_report.py
@@ -6,23 +6,29 @@ class SaleReport(models.Model):
 
 
     confirmation_date_no_time = fields.Date()
+    date_order_date = fields.Date()
+    sale_order_date  = fields.Date(string="Request Date")
 
     def _select(self):
         select_str = super(SaleReport, self)._select()
         select_str += """
-            , s.confirmation_date_no_time as confirmation_date_no_time
+
+            , s.confirmation_date_no_time as confirmation_date_no_time,
+              s.date_order_date as date_order_date,
+              s.sale_order_date as sale_order_date
             """
         return select_str
 
     def _group_by(self):
         group_by_str = super(SaleReport, self)._group_by()
-        group_by_str += ", s.confirmation_date_no_time"
+        group_by_str += ", s.confirmation_date_no_time, s.date_order_date, s.sale_order_date"
         return group_by_str
    
     @api.model
-    def fields_get(self, fields=None):
-        hide = ['confirmation_date']
+    def fields_get(self, fields=None, attributes=None):
+        hide = ['confirmation_date','date']
         res = super(SaleReport, self).fields_get()
         for field in hide:
             res[field]['selectable'] = False
+            res[field]['store'] = False
         return res
