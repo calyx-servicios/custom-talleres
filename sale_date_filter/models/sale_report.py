938163eff1b18b04ab3055c9eabc8dfbb015771a
lucianobaleani <lucianobaleani94@gmail.com>
2022-09-06 11:54:29 -0300

    [ODOO-4563][ADD][sale_date_filter][UPD][search_assistant_custom]

---
+++ sale_date_filter/models/sale_report.py
@@ -0,0 +1,34 @@
+from odoo import models, fields, api
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+
+    confirmation_date_no_time = fields.Date()
+    date_order_date = fields.Date()
+    sale_order_date  = fields.Date(string="Request Date")
+
+    def _select(self):
+        select_str = super(SaleReport, self)._select()
+        select_str += """
+
+            , s.confirmation_date_no_time as confirmation_date_no_time,
+              s.date_order_date as date_order_date,
+              s.sale_order_date as sale_order_date
+            """
+        return select_str
+
+    def _group_by(self):
+        group_by_str = super(SaleReport, self)._group_by()
+        group_by_str += ", s.confirmation_date_no_time, s.date_order_date, s.sale_order_date"
+        return group_by_str
+   
+    @api.model
+    def fields_get(self, fields=None, attributes=None):
+        hide = ['confirmation_date','date']
+        res = super(SaleReport, self).fields_get()
+        for field in hide:
+            res[field]['selectable'] = False
+            res[field]['store'] = False
+        return res
--- search_assistant_custom/models/sale_order.py
+++ search_assistant_custom/models/sale_order.py
@@ -1,13 +1,7 @@
-from odoo import models, api, fields, _
-from odoo.exceptions import ValidationError
+from odoo import models, fields
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    date_order_date = fields.Date(string="Fecha Presupuesto", compute='_compute_date_order_date', store=True)
-
-    @api.multi
-    def _compute_date_order_date(self):
-        for order in self:
-            order.date_order_date = order.date_order
-
+    date_order_date = fields.Date()
