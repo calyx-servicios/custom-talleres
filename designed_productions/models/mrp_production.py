f4a68fdefb268ca21e74f027d9aad47e4476f7bf
lucianobaleani <lucianobaleani94@gmail.com>
2022-09-30 13:21:52 -0300

    [ODOO-5009][FIX][designed_productions]Modifications to fix the error when trying to confirm a sale order

--- designed_productions/models/mrp_production.py
+++ designed_productions/models/mrp_production.py
@@ -12,7 +12,7 @@ class MrpProduction(models.Model):
     def create(self, vals):
         res = super(MrpProduction, self).create(vals)
         if len(res.sale_id.production_ids)>1:
-            line = res.sale_id.order_line[len(res.sale_id.production_ids)-1]
-            res.design = line.to_design
+            if res.sale_id.order_line[len(res.sale_id.production_ids)-1]:
+                line = res.sale_id.order_line[len(res.sale_id.production_ids)-1]
+                res.design = line.to_design
         return res
-        
