370ed616c4ae0397d446c97141446379249dfd87
Gianella <gianellaalvarezmori@gmail.com>
2022-10-11 16:59:08 -0300

    [ODOO-4960][ADD][stock_changes_list_views] 	-black color modification from transfer

---
+++ stock_changes_list_view/__manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Changes List",
+    "summary": """
+        Modify the records that are in red
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["<Github Username/s>"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "11.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": ["base", "stock"],
+     'data': [
+         'views/stock_view_changes.xml',
+     ],
+
+}
---
+++ stock_changes_list_view/models/__init__.py
@@ -0,0 +1,1 @@
+from . import stock_list_changes
