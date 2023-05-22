4d06b7f43ac89be5c100b53e01976cdaf172536c
gabbiiperez <gabbiiperez94@gmail.com>
2023-05-22 19:00:21 -0300

    [ODOO-6589][IMP][tags_report] - IMP tags for products.

---
+++ tags_report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Tags Report",
+    "summary": """
+        This module adds the tags to 
+        the manufacturing report.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["PerezGabriela"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "version": "11.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["mrp","product_attributes", "web"],
+    "data": [
+        "report/report_tags.xml",
+        "report/mrp_production_reports.xml",
+        "views/product_attribute_view.xml"
+    ]
+}
