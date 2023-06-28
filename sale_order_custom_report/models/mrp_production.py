9f4d06f2bc484fe00e9985c27643fc781f462c72
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-28 13:32:48 -0300

    [ODOO-6758][FIX][sale_order_custom_report] - Del unused import

--- sale_order_custom_report/models/mrp_production.py
+++ sale_order_custom_report/models/mrp_production.py
@@ -1,9 +1,6 @@
 from odoo import models, fields, api
 from datetime import timedelta
 import dateutil.parser
-import logging
-_logger = logging.getLogger(__name__)
-
 
 
 class MrpProduction(models.Model):
