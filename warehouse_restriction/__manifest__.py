16c213595d29ca9167e040a9094677b6bb92b56d
lucianobaleani94 <lucianobaleani94@gmail.com>
2023-07-11 17:36:46 -0300

    [ODOO-6882][UPD][warehouse_restriction]

--- warehouse_restriction/__manifest__.py
+++ warehouse_restriction/__manifest__.py
@@ -12,7 +12,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Sale",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
