27e5bfee44a2374e9d59e38b76550620ccd8df66
matias paradiso <matiaskevinparadiso@gmail.com>
2024-02-12 17:21:26 -0300

    [NEW][stock_quant_report]
    Three new fields are added to stock reports

---
+++ stock_quant_report/models/stock_quant.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    real_stock = fields.Float(string='Real Stock',compute='_compute_stock',store=True)
+
+    @api.depends('quantity', 'reserved_quantity')
+    def _compute_stock(self):
+        for stock in self:
+            stock.real_stock = stock.quantity * stock.reserved_quantity
+
+    
